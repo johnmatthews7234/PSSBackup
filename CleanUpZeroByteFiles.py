29e3c81f0d5960fd26b1d03b38f280b78528a9b5
johnmatthews7234 <john.matthews72@gmail.com>
2018-07-04 10:36:26 +1000

    Added ScanforZeroBytes.py

---
+++ CleanUpZeroByteFiles.py
@@ -0,0 +1,21 @@
+import pathlib
+import os
+import argparse
+
+def ScanThisDirectory(dirPath):
+    with os.scandir(dirPath) as it:
+        for entry in it:
+            if entry.is_file() and (os.stat(entry.name).st_size == 0):
+                print("::".join(("Removing", entry.path, entry.name)))
+                #os.remove(entry.name)
+            if entry.is_dir():
+                ScanThisDirectory(entry.path)
+                
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--Path", help="Root path to scan")
+    args = parser.parse_args()
+    ScanThisDirectory(args.Path)
+    
+if __name__ == '__main__' :
+    main()
