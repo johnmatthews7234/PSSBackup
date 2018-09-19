e02a3998b4c52fdcb0f9c6bf0d21690fa061d7fa
johnmatthews7234 <john.matthews72@gmail.com>
2018-09-19 13:14:23 +1000

    Added share mapping to upload shell script

--- clientserver.upload.daily.sh
+++ clientserver.upload.daily.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 PATH=/Library/Frameworks/Python.framework/Versions/3.6/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
-rm ./ClientServer.log
-python3 UploadFilesToDrive.py --Folder ClientServer --Path "/Volumes/Client Server/" --LogFile ClientServer.log
+SERVERNAME="surface-john"
+SHARE="Client Server"
+PLAYGROUND=$(security find-generic-password -a ${USER} -s $SERVERNAME -w)
+if [-e $1.log]
+then
+	rm ./$1
+fi
+if [!-d ./$1]
+then
+	md ./$1
+fi
+mount_smbfs //${USER}:$PLAYGROUND@$SERVERNAME/$SHARE/$1 ./$1
+python3 UploadFilesToDrive.py --Folder $1 --Path ./$1 --LogFile $1.log --RootFolder $SHARE
+
+
+
+
+
 
